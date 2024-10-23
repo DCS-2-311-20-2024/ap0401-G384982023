7f44b7baf8121d12d4c71e7b3d5527e6084d4d47
G384982023 <134253761+G384982023@users.noreply.github.com>
2024-10-23 17:51:57 +0900

    第4回前半

--- ap0401.js
+++ ap0401.js
@@ -1,6 +1,6 @@
 //
 // 応用プログラミング 第4回 課題 (ap0401)
-// G384002023 拓殖太郎
+// G384982023 山﨑凰
 //
 "use strict"; // 厳格モード
 
@@ -25,7 +25,7 @@ function init() {
 
   // 座標軸の設定
   const axes = new THREE.AxesHelper(18);
-  scene.add(axes);
+  //scene.add(axes);
 
   // ブロック数のカウント
   let nBrick = 0
@@ -55,6 +55,13 @@ function init() {
   let vz = -Math.cos(pi / 4);
 
   function moveBall(delta) {
+    if(ballLive){
+    vBall.set(vx , 0, vz)
+    ball.position.addScaledVector(vBall, delta * speed);
+    }else{
+      ball.position.x = paddle.position.x;
+      ball.position.z = paddle.position.z - (paddleR + ballR);
+    }
   }
 
   // ボールの死活
@@ -63,10 +70,14 @@ function init() {
 
   // ボールを停止する
   function stopBall() {
+    speed = 0;
+    ballLive = false;
   }
 
   // ボールを動かす
   function startBall() {
+    ballLive = true;
+    speed = 10;
   }
 
   // マウスクリックでスタートする
@@ -96,24 +107,47 @@ function init() {
       new THREE.BoxGeometry(vFrameW, vFrameH, vFrameD),
       new MeshPhongMaterial({ color: 0xB3B3B3 })
     );
+    lFrame.position.x = (hFrameW - vFrameW) / 2;
+    scene.add(lFrame);
 
     //   右の枠
-
+    const rFrame = new THREE.Mesh(
+      new THREE.BoxGeometry(vFrameW, vFrameH, vFrameD),
+      new MeshPhongMaterial({ color: 0xB3B3B3 })
+    );
+    rFrame.position.x = -(hFrameW - vFrameW) / 2;
+    scene.add(rFrame);
   }
 
   // 壁で反射させる
   const hLimit = hFrameW / 2 - vFrameW;
   const vLimit = vFrameD / 2;
   function frameCheck() {
+    
     // 右
-
+    if(ball.position.x + ballR > hLimit){
+      ball.position.x = hLimit - ballR;
+      vx = -Math.abs(vx);
+    }
     // 左
-
+    if (ball.position.x - ballR < -hLimit) {
+      ball.position.x = -hLimit + ballR;
+      vx = Math.abs(vx);
+    }
     // 上
-
+    if (ball.position.z - ballR < -vLimit) {
+      ball.position.z = -vLimit + ballR;
+      vz = Math.abs(vz);
+    }
     // 下
-
+    if (ball.position.z + ballR > vLimit) {
+      //ball.position.z = vLimit - ballR;
+      //vz = -Math.abs(vz);
+      stopBall();
+    }
   }
+    
+    
 
   // パドル ーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーーー
   // パドルの作成
@@ -126,18 +160,29 @@ function init() {
       new THREE.CylinderGeometry(paddleR, paddleR, paddleL, nSeg),
       new THREE.MeshPhongMaterial({ color: 0x333333, shininess: 100, specular: 0x404040 })
     );
+    center.rotation.z = Math.PI / 2;
+    paddle.add(center);
 
     // パドル端
     const sideGeometry
       = new THREE.SphereGeometry(paddleR, nSeg, nSeg, Math.PI / 2, Math.PI);
     const sideMaterial
       = new THREE.MeshPhongMaterial({ color: 0x666666, shininess: 100, specular: 0xa0a0a0 })
+
     // パドル端(右)
+    const right = new THREE.Mesh(sideGeometry, sideMaterial);
+    right.position.x = paddleL / 2;
+    paddle.add(right);
 
     // パドル端(左)
-
+    const left = right.clone();
+    left.rotation.z = Math.PI;
+    left.position.x = -paddleL / 2;
+    paddle.add(left);
+    
     // パドルの配置
-
+    paddle.position.z = 0.4 * vFrameD;
+    scene.add(paddle)
   }
   
 
@@ -148,13 +193,36 @@ function init() {
     const raycaster = new THREE.Raycaster();
     const intersects = new THREE.Vector3();
     function paddleMove(event) {
-  
+      
+      mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+      raycaster.setFromCamera(mouse, camera);
+      raycaster.ray.intersectPlane(plane, intersects);
+      const offset = hFrameW / 2 - vFrameW - paddleL / 2 - paddleR;
+      if (intersects.x < -offset) {
+        intersects.x = -offset;
+      } else if (intersects.x > offset) {
+        intersects.x = offset;
+      }
+      paddle.position.x = intersects.x;
     }
     window.addEventListener("mousemove", paddleMove, false);
   }
 
   // パドルの衝突検出
   function paddleCheck() {
+    if(Math.abs(ball.position.z - paddle.position.z) < paddleR + ballR &&
+       Math.abs(ball.position.x - paddle.position.x) < paddleR / 2 + ballR){
+
+        if(ball.position.z < paddle.position.z){
+          vz = -Math.abs(vx);
+        }
+        if(ball.position.x >paddle.position.x + paddleL / 2){
+          vx = Math.abs(vx);
+        }
+        else if(ball.position.x < paddle.position.x + paddleR / 2){
+          vz = -Math.abs(vx);
+        }
+       }
   
   }
 
